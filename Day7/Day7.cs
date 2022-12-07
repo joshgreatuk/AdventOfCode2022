b9c5cb888de84c6c4b6d998fa1b48190ea1ab6c5
Joshua Bissell <joahgreat12@gmail.com>
2022-12-07 22:18:39 +0000

    Day 7
    Fixed it

--- Day7/Day7.cs
+++ Day7/Day7.cs
@@ -24,69 +24,94 @@ namespace AOC22
             string[] solution = new string[2];
 
             //Solution
-            //We will read the list of commands to make a directory tree, then process that directory
-            Dictionary<string, Directory> dirList = new Dictionary<string, Directory>();
-            Directory currentDir = new Directory("/");
-            dirList.Add("/", currentDir);
 
+            //Read the directory tree
+            Directory rootDirectory = new Directory("/");
+            Directory currentDirectory = rootDirectory;
             for (int i=0; i < fileInput.Length; i++)
             {
-                string[] line = fileInput[i].Split(' ');
+                string[] line = fileInput[i].Split(" ");
                 if (line[0] == "$")
                 {
                     if (line[1] == "cd")
                     {
-                        if (line[2] == ".." && currentDir.parentDir != string.Empty) 
+                        if (line[2] == ".." && currentDirectory.parentDir != null)
                         {
-                            if (!currentDir.addedToParent)
-                            {
-                                dirList[currentDir.parentDir].dirSize += currentDir.dirSize;
-                                currentDir.addedToParent = true;
-                            }
-                            currentDir = dirList[currentDir.parentDir];
+                            currentDirectory = currentDirectory.parentDir;
                         }
-                        else if (line[2] != "..") 
+                        else if (line[2] != ".." && currentDirectory.dirList.Count > 0)
                         {
-                            if (currentDir.parentDir != string.Empty && !currentDir.addedToParent) 
-                            {
-                                dirList[currentDir.parentDir].dirSize += currentDir.dirSize;
-                                currentDir.addedToParent = true;
-                            }
-                            currentDir = dirList[line[2]];
+                            currentDirectory = (Directory)currentDirectory.dirList.Aggregate((result, element) => result = (element.dirName == line[2]) ? element : result);
                         }
                     }
                 }
-                else if (line[0] == "dir") 
+                else if (line[0] == "dir")
                 {
-                    if (!dirList.ContainsKey(line[1])) dirList.Add(line[1], new Directory(line[1], currentDir.dirName));
+                    currentDirectory.dirList.Add(new Directory(line[1], currentDirectory));
                 }
-                else 
+                else
                 {
-                    currentDir.dirSize += Int32.Parse(line[0]);
-                    currentDir.fileList.Add(string.Join(" ", line));
-                    currentDir.addedToParent = false;
+                    currentDirectory.fileList.Add(string.Join(" ", line));
+                    currentDirectory.dirSize += Int32.Parse(line[0]);
                 }
             }
-            int totalSize = 0;
-            foreach (Directory dir in dirList.Select(element => element.Value))
-            {
-                if (dir.dirSize <= 100000) totalSize += dir.dirSize;
-            }
-            solution[0] = totalSize.ToString();
+
+            //Go through tree and add directories together
+            rootDirectory.dirSize += AddDirsTogether(rootDirectory);
+
+            //Go through tree again to get what we want
+            solution[0] = FindTotalOfDirsBelow(rootDirectory, 100000).ToString();
+            int totalSpace = 70000000;
+            int freeSpace = totalSpace - rootDirectory.dirSize;
+            int neededSpace = 30000000 - freeSpace;
+            solution[1] = FindDirsAbove(rootDirectory, neededSpace).Aggregate((result, element) => result = (element.dirSize < result.dirSize) ? element : result).dirSize.ToString();
 
             return solution;
+        }
+
+        int AddDirsTogether(Directory rootElement)
+        {
+            int totalSize = 0;
+            foreach (Directory dir in rootElement.dirList)
+            {
+                if (dir.dirList.Count > 0) dir.dirSize += AddDirsTogether(dir);
+                totalSize += dir.dirSize;
+            }
+            return totalSize;
+        }
+
+        int FindTotalOfDirsBelow(Directory rootElement, int maxSize)
+        {
+            int totalSize = 0;
+            foreach (Directory dir in rootElement.dirList)
+            {
+                if (dir.dirList.Count > 0) totalSize += FindTotalOfDirsBelow(dir, maxSize);
+                if (dir.dirSize <= maxSize) totalSize += dir.dirSize;
+            }
+            return totalSize;
+        }
+
+        List<Directory> FindDirsAbove(Directory rootElement, int minSize)
+        {
+            List<Directory> dirs = new List<Directory>();
+            foreach (Directory dir in rootElement.dirList)
+            {
+                if (dir.dirList.Count > 0) dirs.AddRange(FindDirsAbove(dir, minSize));
+                if (dir.dirSize > minSize) dirs.Add(dir);
+            }
+            return dirs;
         }
     }
 
     public class Directory
     {
         public string dirName = "";
-        public string parentDir = "";
-        public bool addedToParent = false;
+        public Directory? parentDir = null;
+        public List<Directory> dirList = new List<Directory>();
         public List<string> fileList = new List<string>();
         public int dirSize = 0;
 
-        public Directory(string name, string parent="")
+        public Directory(string name, Directory? parent=null)
         {
             dirName = name;
             parentDir = parent;
